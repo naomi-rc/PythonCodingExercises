1f15f8c20b088cde851c688651287597a956c9b5
Naomi Catwell <naomi.catwell.1@etsmtl.net>
2021-01-03 12:40:15 -0500

    completed challenge 3 - raindrops

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,5 +1,8 @@
 FACTORS = {3: "Pling", 5: "Plang", 7: "Plong"}
 
 def convert(number):    
-    result = ''.join(sound for factor, sound in FACTORS.items() if number % factor == 0)
+    result = ''.join(
+        sound for factor, sound in FACTORS.items() 
+        if number % factor == 0
+    )
     return result if result else str(number)
