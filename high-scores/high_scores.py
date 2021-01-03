5746c4b10ef114ddd68d1aa637cabf6bc27234ec
Naomi Catwell <naomi.catwell.1@etsmtl.net>
2021-01-03 13:46:25 -0500

    add gitignore; improve challenge 4 - high_scores

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,18 +1,10 @@
 def latest(scores):
-    return scores[len(scores)-1]
+    return scores[-1]
 
 
 def personal_best(scores):
     return max(scores)
 
 
-def personal_top_three(scores):
-    top_scores_count = 3
-    top_three = []
-    top_scores_count = top_scores_count if len(scores) > top_scores_count else len(scores)
-
-    for i in range(top_scores_count):
-        max_score = max(scores)
-        top_three.append(max_score)
-        scores.remove(max_score)
-    return top_three
+def personal_top_three(scores):    
+    return sorted(scores, reverse=True)[:3]
