25ae5692424d86c43c742e2cb859b400509ed0a3
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-01-07 21:13:34 -0500

    first solution to challenge 2.3 - protein translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,23 @@
+STOP = "STOP"
+CORRESPONDANCES = {"Methionine" : "AUG", "Phenylalanine" : "UUU, UUC", 
+"Leucine" : "UUA, UUG", "Serine" : "UCU, UCC, UCA, UCG", 
+"Tyrosine" : "UAU, UAC", "Cysteine" : "UGU, UGC", "Tryptophan" : "UGG", 
+STOP : "UAA, UAG, UGA"}
+
+def proteins(strand):
+    proteins = []
+    result = []
+    strand_codons = [strand[i:i+3] for i in range(0, len(strand), 3)]
+    for codon in strand_codons:
+        #proteins.append(protein for protein, codons in CORRESPONDANCES.items() if codon in codons)
+        #proteins_list += ''.join(":"+protein for protein, codons in CORRESPONDANCES.items() if codon in codons)
+        proteins += (protein for protein, codons in CORRESPONDANCES.items() if codon in codons)
+        
+    for protein in proteins:
+        if protein == STOP:
+            break
+        result.append(protein)   
+    return result   
+
+
+print(proteins("AUGUUUUCUUAAAUG"))
