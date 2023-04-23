71fa93517efea7e75246d99c76f37af1c265c9e5
naomi-rc <naomi.cat182128@gmail.com>
2023-04-23 15:55:29 -0400

    save progress

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,9 +1,25 @@
 class Matrix:  
     def __init__(self, matrix_string):
-        self.matrix = matrix_string.split('\n')
+        self.matrix = matrix_string.splitlines()
+        self.rows = list(list(int(value) for value in row.split()) for row in self.matrix) #[[9 8 7], [5 3 2], [6 6 7]]
+        #self.columns = list(list(int((value.split())[index-1]) for value in self.matrix)) #[[9 5 6], [8 3 6], [7 2 7]]
+        print(self.matrix)
 
     def row(self, index):
-        return list(int(value) for value in self.matrix[index-1].split())
+        #return list(int(value) for value in self.matrix[index-1].split())
+        return self.rows[index-1]
 
     def column(self, index):
-        return list(int((value.split())[index-1]) for value in self.matrix)
+        #return list(int((value.split())[index-1]) for value in self.matrix)
+        return self.columns[index-1]
+
+
+
+
+matrix_string = "9 8 7 \n 5 3 2 \n 6 6 7"
+#matrix = Matrix(matrix_string)
+#print(matrix.row(1))
+
+a = [1, 2, 3]
+for i, v in enumerate(a):
+    print(f"{i} - {v}")
--- word-count/word_count.py
+++ word-count/word_count.py
@@ -8,7 +8,7 @@ def count_words(sentence):
     result = {}
     sentence = replace_punctuation(sentence)
     sentence = sentence.lower()
-    words_within_apostrophes = re.findall(r"([\"\']\w+[\"\'])", sentence) #trying to replace instances of words wrapped in quotes with the actual word
+    words_within_apostrophes = re.findall(r"([\"\']\w+[\"\'])", sentence) 
     
     for word in words_within_apostrophes:
         replacement =  word.replace("'", "")
@@ -19,14 +19,9 @@ def count_words(sentence):
     
     for word in words:
         if word not in string.whitespace:
-            #print
             if word.count(APOSTROPHE) > 1:
-                #print(word)
                 word = word.replace(APOSTROPHE, "")
-                
-            #print(word)
-            #print(r"\b"+ word +r"\b")
-            
+                            
             result.update({str(word) : len(re.findall(r"\b"+ word +r"\b", sentence))})   
 
     return result
@@ -39,22 +34,3 @@ def replace_punctuation(sentence):
      
 
 
-
-
-def count_words2(sentence):
-    result = {}
-    for symbol in PUNCTUATION + string.whitespace:
-        sentence = sentence.replace(symbol, DELIMETER)
-    word_set = sentence.split(DELIMETER)
-    print(word_set)
-    for word in word_set:
-        if word:
-            result.update({word : word_set.count(word) })
-    return result
-
-#print(count_words("''hey''"))
-print(count_words("car: carpet as java: javascript!!&@$%^&"))
-
-#print(str("''hey''").replace("'", ""))
-#print("''hey''".replace("'", ""))
-#print(string.punctuation)
